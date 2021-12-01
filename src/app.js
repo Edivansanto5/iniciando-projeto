81ba54aaea5d55463c45a4e8ff14b78b435f734d
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2021-12-01 17:43:18 -0300

    roteando as rotas das requisiçao

--- src/app.js
+++ src/app.js
@@ -3,8 +3,25 @@ const express = require('express')
 const app = express()
 
 // fazendo as rotas
-app.use('/',(req, res)=>{
+app.get('/',(req, res)=>{
     res.send('App online e funcionado ok marujo !')
+
+})
+
+app.get('/alunos',(req, res)=>{
+    res.send('Retorna todos os alunos !')
+
+})
+app.post('/alunos',(req, res)=>{
+    res.send('Adicionar Aluno  !')
+
+})
+app.put('/alunos',(req, res)=>{
+    res.send('Alterar um  Aluno especifico !')
+
+})
+app.delete('/alunos',(req, res)=>{
+    res.send('Deletar um  Aluno especifico !')
 
 })
 
