304b8ab7cfe40aa02a72af3ef1e07b63747cf145
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2021-12-01 17:25:50 -0300

    fazendo as rotas

--- src/app.js
+++ src/app.js
@@ -1,4 +1,13 @@
-const express = Require('express')
+const express = require('express')
 
 const app = express()
-app.listen(3000)
+
+// fazendo as rotas
+app.use('/',(req, res)=>{
+    res.send('App online e funcionado ok marujo !')
+
+})
+
+app.listen(3000,()=>{
+    console.log('App online')
+})
